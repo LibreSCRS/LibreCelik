5b4d0266475cdd5afa5f3ba8ecbcd17b6a1d73cd
hirashix0 <hirashix0@proton.me>
2026-03-04 23:21:56 +0100

    Fix Qt <6.7 compatibility: add QDebug<<std::string overload to logging header
    Qt 6.7 added QDebug::operator<<(std::string) as a member. In Qt 6.4.2
    (Ubuntu 24.04), std::string is ambiguous between QUtf8StringView and
    QByteArrayView. Providing a free function overload (guarded to Qt<6.7)
    in libreceliklog.h resolves this project-wide without touching each call site.

--- src/utils/libreceliklog.h
+++ src/utils/libreceliklog.h
@@ -4,7 +4,19 @@
 #ifndef libreceliklog_H
 #define libreceliklog_H
 
+#include <QDebug>
 #include <QLoggingCategory>
+#include <string>
+
+// Qt 6.7+ adds QDebug::operator<<(std::string) as a member; older versions are
+// ambiguous (std::string matches both QUtf8StringView and QByteArrayView).
+#if QT_VERSION < QT_VERSION_CHECK(6, 7, 0)
+inline QDebug operator<<(QDebug dbg, const std::string& s)
+{
+    return dbg << QString::fromStdString(s);
+}
+#endif
+
 Q_DECLARE_LOGGING_CATEGORY(libreSCRSGeneral)
 Q_DECLARE_LOGGING_CATEGORY(librecSCRSCard)
 Q_DECLARE_LOGGING_CATEGORY(libreCelikAPI)
